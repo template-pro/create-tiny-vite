19f07c8ee350ad4a45519220cb71315c0610a1ac
wuxh <wxh1220@gmail.com>
2023-01-08 06:49:19 +0800

    update

--- index.mjs
+++ index.mjs
@@ -1,18 +1,10 @@
 #!/usr/bin/env node
 
-import { createRequire } from 'module';
-import { spawnSync } from 'child_process'
 import { readFile } from 'fs/promises';
 
 const pkg = JSON.parse(await readFile(new URL('package.json', import.meta.url)));
-const require = createRequire(import.meta.url);
+console.log(`Welcome to ${pkg.name}@${pkg.version}\r\n`);
 
-console.log(`Welcome to ${pkg.name}@${pkg.version}`);
-
-process.exit(
-  spawnSync(
-    require.resolve('create-vite'),
-    ...process.argv.slice(2),
-    { stdio: 'inherit' }
-  ).status
-)
+; (async () => {
+  await import('./dist/index.mjs');
+})();
